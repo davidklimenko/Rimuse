f85e2527e150b1720b5ebb8c52962760fdf5c26a
David <38540487+David200332@users.noreply.github.com>
2021-04-24 21:11:31 +0300

    1/2 player create

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,10 +1,21 @@
 import react from 'react'
 import style from './Player.module.css'
+import logo from '../../img/Song__Logo-player.png'
 
 export default function Player(){
     return (
         <div className={style.player}>
-            <h1>Player</h1>
+            <div className={style.upperContent}>
+                <div className={style.upper_title}>
+                    <h1>104 - НЕ ЖАЛЬ ft. Скриптонит</h1>
+                </div>
+                <div className={style.upper_photo}>
+                    <img src={logo} alt="image not found"/>
+                </div>
+            </div>
+            <div className={style.lowerContent}>
+
+            </div>
         </div>
     )
 }
