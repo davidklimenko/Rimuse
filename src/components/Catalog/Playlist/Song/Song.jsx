4118b3e38401d7c9aa5485e5f3272d85714bc731
David <38540487+David200332@users.noreply.github.com>
2021-04-24 17:24:14 +0300

    made song

---
+++ src/components/Catalog/Playlist/Song/Song.jsx
@@ -0,0 +1,34 @@
+import react from 'react'
+import style from './Song.module.css'
+import logo from '../../../../img/Song_Logo.png'
+import detail__icon from '../../../../img/detail-icon.svg'
+
+
+export default function Song(){
+    return (
+        <div className={style.song}>
+            <div className={style.song__left}>
+                <div className={style.song__left_img}>
+                    <img src={logo} alt="image not found"/>
+                </div>
+
+                <div className={style.song__left_text}>
+                    <h1>НЕ ЖАЛЬ ft. Скриптонит</h1>
+                    <div className={style.song__left_text_detail}>
+                        <span> MP3 </span> |
+                        <span> 44kHz </span> |
+                        <span> 128kbps </span> |
+                        <span> 7.60 MB </span>
+                    </div>
+                </div>
+            </div>
+
+            <div className={style.song__right}>
+                <span className={style.song__right_time}>
+                    <h2>03:35</h2>
+                </span>
+                <img src={detail__icon} alt="image not found"/>
+            </div>
+        </div>
+    )
+}
